b658226a771f6d45e96809664c160da3cc60ba1d
김서현 <122426329+twotthh@users.noreply.github.com>
2025-08-11 02:20:04 +0900

    Create palette.dart

---
+++ lib/config/palette.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/painting.dart';
+
+class Palette {
+  static const Color iconColor = Color(0xFFD4D6D5);
+  static const Color activeColor = Color(0xFF937EA8);
+  static const Color textColor1 = Color(0XFFD4D5D6);
+  static const Color textColor2 = Color(0XFF1A1A1A);
+  static const Color googleColor = Color(0xFF848C63);
+  static const Color backgroundColor = Color(0xFFBEC8AF);
+}
