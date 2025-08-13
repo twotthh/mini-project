ce0643988003d9a39d1bc64ef8d9ef41f4865063
김서현 <122426329+twotthh@users.noreply.github.com>
2025-08-13 21:36:14 +0900

    Update message.dart

--- lib/chatting/component/message.dart
+++ lib/chatting/component/message.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:project_mini/chatting/component/chat_bubble.dart';
+import 'package:heycoun/chatting/component/chat_bubble.dart';
 
 class Messages extends StatelessWidget {
   final List<Map<String, dynamic>> messages;
   final String currentUserId;
 
-  const Messages({Key? key, required this.messages, required this.currentUserId})
-      : super(key: key);
+  const Messages({
+    Key? key,
+    required this.messages,
+    required this.currentUserId,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
       reverse: true,
       itemCount: messages.length,
       itemBuilder: (context, index) {
@@ -19,7 +23,7 @@ class Messages extends StatelessWidget {
           chat['text'] ?? '',
           chat['userID'] == currentUserId,
           chat['userName'] ?? '',
-          chat['userImage'] ?? 'image/eggtart.jpg',
+          chat['userImage'] ?? 'images/eggtart.jpg',
         );
       },
     );
