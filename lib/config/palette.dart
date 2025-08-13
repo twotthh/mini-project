39508245050c2343af8d49e527da032a66f904ed
김서현 <122426329+twotthh@users.noreply.github.com>
2025-08-13 21:35:33 +0900

    Update palette.dart

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/painting.dart';
 
 class Palette {
-  static const Color iconColor = Color(0xFFD4D6D5);
-  static const Color activeColor = Color(0xFF937EA8);
-  static const Color textColor1 = Color(0XFFD4D5D6);
-  static const Color textColor2 = Color(0XFF1A1A1A);
-  static const Color googleColor = Color(0xFF848C63);
-  static const Color backgroundColor = Color(0xFFBEC8AF);
+  static const Color backgroundColor1 = Color(0xFFD4D6D5);
+  static const Color backgroundColor2 = Color(0xFFBEC8AF);
+  static const Color mainColor = Color(0xFF937EA8);
+  static const Color mainColor2 = Color(0xFF81758C);
+  static const Color textColor_black = Color(0XFF1A1A1A);
+  static const Color textColor_white = Color(0XFFFAFAFA);
+  static const Color pointColor = Color(0xFF848C63);
 }
